25e2354019bf184829e982f672d8fe00cf2b8254
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-04-29 20:42:54 +0100

    clean up test a bit

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,12 @@ mod test {
     use super::checked;
 
     #[test]
-    pub fn test_basic_use() {
+    fn test_basic_use() {
         let result = checked!(1_u64 + 2 + 3).unwrap();
         assert_eq!(result, 6);
 
         let x = u64::MAX - 1;
         let result = checked!(x + 1 + 1).unwrap_err();
-        println!("{result}");
         assert_eq!(result, "Failure in: x + 1  》+《  1");
 
         let result = checked!(-1_i64).unwrap();
